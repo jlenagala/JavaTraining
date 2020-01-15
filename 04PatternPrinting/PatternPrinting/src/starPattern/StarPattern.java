5877a1e5a63a402aea6a20571330cd12b8ea9128
jlenagala <jananianut@gmail.com>
2020-01-15 19:28:28 +0530

    Created Number pattern class and updated 2nd for loop in star pattern class

--- 04PatternPrinting/PatternPrinting/src/starPattern/StarPattern.java
+++ 04PatternPrinting/PatternPrinting/src/starPattern/StarPattern.java
@@ -3,20 +3,33 @@ package starPattern;
 public class StarPattern {
 
 	public static void main(String[] args) {
-		
-		for(int i=0;i<6;i++) {
-			for(int j=0;j<=i;j++) {
-			System.out.print("*");
+
+		for (int i = 1; i <= 7; i++) {
+
+			for (int j = 7; j >= i; j--) {
+				System.out.print(" ");
 			}
+
+			for (int k = 1; k <= (2 * i) - 1; k++) {
+				System.out.print("*");
+			}
+
 			System.out.println();
 		}
-		
-		for(int i=6;i>=0;i--) {
-			for(int j=0;j<=i;j++) {
-			System.out.print("*");
+
+		for (int i = 6; i >= 1; i--) {
+
+			for (int j = 7; j >= i; j--) {
+				System.out.print(" ");
 			}
+
+			for (int k = 1; k < (2 * i); k++) {
+				System.out.print("*");
+			}
+			
 			System.out.println();
 		}
+
 	}
 
 }
