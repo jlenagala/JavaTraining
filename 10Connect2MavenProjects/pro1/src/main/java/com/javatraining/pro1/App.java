bb679c292b05a064e420c8d12df974bdd5329c01
jlenagala <jananianut@gmail.com>
2020-01-19 17:15:34 +0530

    Connect 2 maven projects using Maven Dependencies

---
+++ 10Connect2MavenProjects/pro1/src/main/java/com/javatraining/pro1/App.java
@@ -0,0 +1,14 @@
+package com.javatraining.pro1;
+import com.javatraining.pro2.test.TestP2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! pro1");
+		com.javatraining.pro2.App.main(args);
+		TestP2.print();
+	}
+}
---
+++ 10Connect2MavenProjects/pro2/src/main/java/com/javatraining/pro2/test/TestP2.java
@@ -0,0 +1,7 @@
+package com.javatraining.pro2.test;
+
+public class TestP2 {
+	public static void print() {
+		System.out.println("Hello World! - pro2");
+	}
+}
