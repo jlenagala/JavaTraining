7299121d926f7c95f4c433a30f9691e56c19c271
jlenagala <jananianut@gmail.com>
2020-01-23 07:09:44 +0530

    collect a stream using list

--- 13StreamsEx/StreamEx/src/StreamExersice/Filter.java
+++ 13StreamsEx/StreamEx/src/StreamExersice/Filter.java
@@ -1,35 +1,33 @@
 
 package StreamExersice;
-import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Filter {
 
 	public static void main(String[] args) {
-		
-		
+
 		HashMap<String, Integer> students = new HashMap<String, Integer>();
-		students.put("Anton",42);
+		students.put("Anton", 42);
 		students.put("Kamala", 77);
-		students.put("Nimala",90);
+		students.put("Nimala", 90);
 		students.put("Anil", 25);
 		students.put("Geetha", 61);
 		students.put("Nayana", 99);
-	
 
-        
-		Map<String, Integer>filterd = 
-				students.entrySet()
-				.stream()
-				.filter((s1->(s1.getValue()>60)))
-				.sorted()
-				.collect(Collectors.toMap(s -> s.getKey(), s -> s.getValue()));
+		List<String> filterd = students.keySet().stream().filter((s) -> students.get(s) > 60).sorted()
+				.collect(Collectors.toList());
 
 		System.out.println(filterd);
+
+		/*
+		 * Map<String, Integer>filterd = students.entrySet() .stream()
+		 * .filter((s1->(s1.getValue()>60)))
+		 * 
+		 * System.out.println(filterd);
+		 */
 	}
 
 }
